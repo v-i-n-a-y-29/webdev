b454b2c9918f2bc9807f93b9a7795a93377d5d8a
v-i-n-a-y-29 <vinaysemwal392@gmail.com>
2025-03-15 01:40:39 +0530

    Add files via upload
    first react static page

---
+++ first-react-project/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./components/Main.jsx"
+import Navbar from "./components/Navbar.jsx"
+import "./App.css"
+export default function App()
+{
+    return(
+        <>
+        <Navbar/>
+        <Main/>
+        
+        </>
+    )
+}
---
+++ first-react-project/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Main()
+{
+    return(
+        <header>
+            <nav>
+                <img src="/react-logo.png" alt="react logo" />
+            </nav>
+            <span>
+                <h1 className="inter-font">ReactFacts</h1>
+            </span>
+        </header>
+    )
+}
